4be112adcbdb5894cfd06d937094386455a21533
diep <xuandiep2389@gmail.com>
2018-12-20 15:28:16 +0700

    initial commit

---
+++ src/main/java/will/quanlythuvien/QuanLyThuVienApplication.java
@@ -0,0 +1,14 @@
+package will.quanlythuvien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyThuVienApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanLyThuVienApplication.class, args);
+    }
+
+}
+
